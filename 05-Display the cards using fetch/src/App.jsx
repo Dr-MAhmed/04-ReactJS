9ab5808d2cc890063e083d70748dde1967023e79
Muhammad Ahmed <dr.muhammadahmed05@gmail.com>
2024-03-19 13:16:36 +0500

    react-router

--- 05-Display the cards using fetch/src/App.jsx
+++ 05-Display the cards using fetch/src/App.jsx
@@ -1,39 +1,56 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Navbar from './components/Navbar';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [cards, setCards] = useState([]);
 
-  const fetchData = async ()=>{
-    let a = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const fetchData = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts");
     let data = await a.json();
     setCards(data);
-    console.log(data)
-  } 
+    console.log(data);
+  };
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
   return (
     <>
-    <Navbar/>
-    <div className="container mx-auto grid grid-cols-4 gap-14">
-  {cards.map((card) => {
-    return (
-      <>
-      <div key={card.id} className="card max-w-[500px] border border-purple-950 ">
-        <img src={"https://wallpapercave.com/wp/wp9109485.jpg"} width={280} height={250} alt={card.title} />
-        <h1><b>Title: </b>{card.title}</h1>
-        <p><b>Body: </b>{card.body}</p>
-        <p><b>userId: </b>{card.userId}</p>
+      <Navbar />
+      <div className="container mx-auto grid grid-cols-4 gap-14">
+        {cards.map((card) => {
+          return (
+            <>
+              <div
+                key={card.id}
+                className="card max-w-[500px] border border-purple-950 "
+              >
+                <img
+                  src={"https://wallpapercave.com/wp/wp9109485.jpg"}
+                  width={280}
+                  height={250}
+                  alt={"Coding Image"}
+                />
+                <h1>
+                  <b>Title: </b>
+                  {card.title}
+                </h1>
+                <p>
+                  <b>Body: </b>
+                  {card.body}
+                </p>
+                <p>
+                  <b>userId: </b>
+                  {card.userId}
+                </p>
+              </div>
+            </>
+          );
+        })}
       </div>
-      </>
-    );
-  })}
-</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 06-React Router/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      Hey i am login page
+    </div>
+  )
+}
+
+export default Login
