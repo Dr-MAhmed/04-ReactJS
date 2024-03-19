dc3e852db600f0bdc67a15c76290f0136f39f904
Muhammad Ahmed <dr.muhammadahmed05@gmail.com>
2024-03-19 17:18:46 +0500

    handling form in react.js

---
+++ 10-Handling form + connecting to express backend/src/App.jsx
@@ -0,0 +1,106 @@
+import "./App.css";
+import { useForm } from "react-hook-form";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const delay = (d) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, d * 1000);
+    });
+  };
+
+  const onSubmit = async (data) => {
+    await delay(2); //Simulating network delay..
+
+    let fet = await fetch("http://localhost:3000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    let res = await fet.text();
+    console.log(data, res);
+    
+    // Custom Errors
+    if (data.username !== "harry") {
+      setError("harry", {
+        message: "This user has been blocked from accessing the website",
+      });
+    }
+    if (data.password !== "harry") {
+      setError("codeWithHarry", {
+        message: "Password should be harry",
+      });
+    }
+  };
+  return (
+    <>
+    <h4>This is about form handling in reactJS and connecting it with express backend.. That was not about styling, That was only about functionality..</h4>
+      {isSubmitting && <div className="fontBold fontSize">Loading....</div>}
+      <div className="container">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="username"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "username is required",
+              },
+              minLength: {
+                value: 3,
+                message: "username must be at least 3 characters",
+              },
+              maxLength: {
+                value: 15,
+                message: "username must be at most 15 characters",
+              },
+            })}
+            type="text"
+          />
+          {errors.username && (
+            <div className="purple">{errors.username.message}</div>
+          )}
+          <br />
+          <input
+            placeholder="password"
+            {...register("password", {
+              required: {
+                value: true,
+                message: "password is required",
+              },
+              minLength: {
+                value: 5,
+                message: "password must be at least 5 characters",
+              },
+              maxLength: {
+                value: 15,
+                message: "password must be at most 15 characters",
+              },
+            })}
+            type="password"
+          />
+          {errors.password && (
+            <div className="purple">{errors.password.message}</div>
+          )}
+          {errors.harry && <div className="purple">{errors.harry.message}</div>}
+          {errors.codeWithHarry && (
+            <div className="purple">{errors.codeWithHarry.message}</div>
+          )}
+          <br />
+          <input disabled={isSubmitting} type="submit" />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
